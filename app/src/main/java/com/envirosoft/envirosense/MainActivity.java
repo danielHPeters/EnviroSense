edaf2ce114eefc15eeaee65ec32cd2ded4f3b855
Daniel Peters <daniel.peters.ch@gmail.com>
2017-08-31 10:51:51 +0200

    Added new activity and buttons

--- app/src/main/java/com/envirosoft/envirosense/MainActivity.java
+++ app/src/main/java/com/envirosoft/envirosense/MainActivity.java
@@ -1,13 +1,26 @@
 package com.envirosoft.envirosense;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    /**
+     * Instantiate an Intent to open the graph activity
+     */
+    public void openGraphWindow(){
+        Intent openGraph = new Intent(this, GraphActivity.class);
+        startActivity(openGraph);
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        Button
+
     }
 }
